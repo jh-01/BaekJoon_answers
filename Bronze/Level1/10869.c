911d4814a5c39c8bcea47e539b368be648d4b50f
jh-01 <79428775+jh-01@users.noreply.github.com>
2022-03-30 17:38:52 +0900

    Create 10869.c

---
+++ Bronze/Level1/10869.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+  // 각각의 사칙연산 출력
+	printf("%d\n", a+b);
+	printf("%d\n", a-b);
+	printf("%d\n", a*b);
+	printf("%d\n", a/b);
+	printf("%d\n", a%b);
+}
