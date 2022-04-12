e773300809aaec52746bc05a5fe2e4ff17b8482d
Jihyeon Kim <79428775+jh-01@users.noreply.github.com>
2022-04-12 23:00:57 +0900

    Create 11021.c

---
+++ Bronze/Level3/11021.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, i, n;
+	scanf("%d", &n);
+	for(i = 1; i <= n; i++){
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", i, a + b);
+	}
+	return 0;
+}
