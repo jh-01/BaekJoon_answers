0ff720dab3f3fb27ffe938542d4d4694e7f53455
Jihyeon Kim <79428775+jh-01@users.noreply.github.com>
2022-04-19 00:12:53 +0900

    Create 11726.c

---
+++ Silver/Level3/11726.c
@@ -0,0 +1,17 @@
+// 다이나믹 프로그래밍
+#include <stdio.h>
+
+int d[1001];
+
+int dp(int x){
+	if(x == 1) return 1;
+	if(x == 2) return 2;
+	if(d[x] != 0) return d[x];
+	return d[x] = (dp(x - 1) + dp(x - 2)) % 10007;
+}
+
+int main(void){
+	int x;
+	scanf("%d", &x);
+	printf("%d", dp(x));
+}
